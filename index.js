2affa3476b7ea7553761023b51bcd898b2359f77
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 21:25:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -86,11 +86,11 @@ function createRock(x) {
 
 function endGame() {
   clearInterval(gameInterval)
-  return alert('YOU LOSE!')
-
   for(let i=0;i<ROCKS.length;i++){
     ROCKS[i].remove()
   }
+  return alert('YOU LOSE!')
+
 document.removeEventListener('keydown', moveDodger)
 }
 
