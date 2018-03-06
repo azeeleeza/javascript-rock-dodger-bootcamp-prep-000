a7de15d0de7b0e5b2e864701066835f1b9de4f0a
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 21:51:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -87,12 +87,10 @@ function moveDodger(e) {
     const arrow = e.which
 
     if (arrow === LEFT_ARROW) {
-
         e.preventDefault()
         e.stopPropagation()
         moveDodgerLeft()
     } else if (arrow === RIGHT_ARROW) {
-
         e.preventDefault()
         e.stopPropagation()
         moveDodgerRight()
